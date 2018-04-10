0906da9508ec8cb3776b22c38c28c5643e9b0e73
Brilyandika <brilyandika.andhana@gmail.com>
2018-04-10 22:43:31 +0700

    html add artikel

--- application/views/dashboard/addartikel.php
+++ application/views/dashboard/addartikel.php
@@ -7,6 +7,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     ?>
 <body>
   <!--/.ASIQUE -->
+  <div class="form-group">
+  <label for="title">Judul:</label>
+  <input type="text" class="form-control" id="title">
+</div>
+<div class="form-group">
+  <label for="author">Author:</label>
+  <input type="text" class="form-control" id="author">
+</div>
+<div class="form-group">
+  <label for="date">Tanggal:</label>
+  <input type="date" class="form-control" id="date">
+</div>
+<div class="form-group">
+  <label for="isiblog">isi blog:</label>
+  <input type="textarea" class="form-control" id="isiblog">
+</div>
+<div class="form-group">
+upload gambar:
+<div class="file btn btn-lg btn-primary">
+							Upload
+							<input type="file" name="file"/>
+						</div>
+</div>
 
 </div>
 </body>
